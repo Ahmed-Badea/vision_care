9d1b3523ad804c43a57dd15b92d4a437853c8b40
Ahmed-Badea <ahmedbadea158@gmail.com>
2023-11-02 03:02:56 +0200

    add Sustainable Development page

--- src/Pages/SustainableDevelopment/SustainableDevelopment.js
+++ src/Pages/SustainableDevelopment/SustainableDevelopment.js
@@ -1,15 +1,43 @@
 import React from 'react'
+import { Container } from 'reactstrap';
+import styled, { keyframes } from 'styled-components';
+import { bounceInDown } from 'react-animations';
 import {dependencies} from '../../Tools/dependencies'
+import EyeIcon from './../../assets/images/eye.jpg'
 import text from './text.json'
+import './SustainableDevelopment.scss'
+
+const bounceInDownAnimation = keyframes`${bounceInDown}`;
+const BouncyInDownDiv = styled.div`
+  animation: 1s ${bounceInDownAnimation};
+`;
 
 function SustainableDevelopment() {
 
   const currentLang = dependencies.getCurrentLang();
   const Text = text[currentLang];
 
+  const renderBody = () => (
+    <div className='development_container'>
+      <Container>
+        <BouncyInDownDiv>
+          <img src={EyeIcon} alt="Eye" />
+        </BouncyInDownDiv>
+        <div className='description'>
+          <p>{Text.description_1}</p>
+          <p>{Text.description_2}</p>
+          <p>{Text.description_3}</p>
+          <p>{Text.description_4}</p>
+          <p>{Text.description_5}</p>
+          <p>{Text.description_6}</p>
+        </div>
+      </Container>
+    </div>
+  )
+
   return (
-    <div style={{"height": "500px", "textAlign": "center", "lineHeight": "500px", "fontSize": "30px"}}>
-      {Text.home}
+    <div>
+      {renderBody()}
     </div>
   )
 }
