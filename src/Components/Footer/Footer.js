72ccb48de6c962e21bd39b8168deb59530d396b0
Ahmed-Badea <ahmedbadea158@gmail.com>
2023-11-05 21:32:44 +0200

    add contact info

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -15,50 +15,62 @@ const Footer = () => {
   const nodeRef = useRef();
   const isVisible = useIsVisible(nodeRef);
 
+  const renderBody = () => (
+    <Container>
+      <Row>
+        <Col md={4}>
+          <h4>{Text.title}</h4>
+          <p>{Text.description}</p>
+          <p>{Text.goal}</p>
+        </Col>
+        <Col md={4} className='social_links'>
+          <h4>{Text.social}</h4>
+          <a href='/'><i className="fa-brands fa-facebook-f"></i></a>
+          <a href='/'><i className="fa-brands fa-instagram"></i></a>
+          <a href='/'><i className="fa-brands fa-x-twitter"></i></a>
+          <a href='/'><i className="fa-brands fa-youtube"></i></a>
+          <div className='contact_info'>
+            <p>
+              <i className="fa fa-map-marker-alt"></i> 
+              {Text.location}
+            </p>
+            <p>
+              <i className="fa fa-paper-plane"></i> 
+              {Text.zip_code} 1 3 7 8 7
+            </p>
+            <p>
+              <i className="fa fa-inbox"></i> 
+              {Text.po_box} 5 0 6 7
+            </p>
+            <a href="tel:+966 11 207 9919">
+              <i className="fa fa-phone"></i>
+              +966 11 207 9919
+            </a>
+            <a href="mailto:info@visioncareksa.com">
+              <i className="fa fa-envelope"></i>
+              info@visioncareksa.com
+            </a>
+          </div>
+          <div>
+            <img ref={nodeRef} className={`logo ${isVisible ? "animate__animated animate__bounce" : ""}`} src={Logo} alt="TALENT VISION CARE" />
+          </div>
+        </Col>
+        <Col md={4} className='routes'>
+          <h4>{Text.quick_links}</h4>
+          {Text.quick_links_routes.map((route) => (
+            <a key={route.path} href={route.path}>{route.name}</a>
+          ))}
+        </Col>
+        <Col md={12}>
+          <p className='copy_right'>{Text.copy_right}</p>
+        </Col>
+      </Row>
+    </Container>
+  )
+
   return (
-    <footer className="footer">
-      <Container>
-        <Row>
-          <Col md={4}>
-            <h4>{Text.title}</h4>
-            <p>{Text.description}</p>
-            <p>{Text.goal}</p>
-          </Col>
-          <Col md={4} className='social_links'>
-            <h4>{Text.social}</h4>
-            <a href='/'><i className="fa-brands fa-facebook-f"></i></a>
-            <a href='/'><i className="fa-brands fa-instagram"></i></a>
-            <a href='/'><i className="fa-brands fa-x-twitter"></i></a>
-            <a href='/'><i className="fa-brands fa-youtube"></i></a>
-            <div className='contact_info'>
-              <p>
-                <i className="fa fa-map-marker-alt"></i> 
-                {Text.location}
-              </p>
-              <a href="tel:+966 11 207 9919">
-                <i className="fa fa-phone"></i>
-                +966 11 207 9919
-              </a>
-              <a href="mailto:info@visioncareksa.com">
-                <i className="fa fa-envelope"></i>
-                info@visioncareksa.com
-              </a>
-            </div>
-            <div>
-              <img ref={nodeRef} className={`logo ${isVisible ? "animate__animated animate__bounce" : ""}`} src={Logo} alt="TALENT VISION CARE" />
-            </div>
-          </Col>
-          <Col md={4} className='routes'>
-            <h4>{Text.quick_links}</h4>
-            {Text.quick_links_routes.map((route) => (
-              <a key={route.path} href={route.path}>{route.name}</a>
-            ))}
-          </Col>
-          <Col md={12}>
-            <p className='copy_right'>{Text.copy_right}</p>
-          </Col>
-        </Row>
-      </Container>
+    <footer>
+      {renderBody()}
     </footer>
   );
 };
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -54,43 +54,47 @@ function Header(args) {
     }
   }
 
+  const renderBody = () => (
+    <Navbar {...args} fixed='top' expand='lg'>
+      <NavbarBrand href="/">
+        <img className='logo' src={Logo} alt='TALENT VISION CARE' />
+      </NavbarBrand>
+      <NavbarToggler onClick={toggle} />
+      <Collapse isOpen={isOpen} navbar>
+        <Nav className="m-auto" navbar>
+          {Text.nav_links.map((ele, index) => (
+            ele.drop_down ?
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  {ele.name}
+                </DropdownToggle>
+                <DropdownMenu right>
+                  {ele.routes.map((item, index) => (
+                    <DropdownItem key={index}>
+                      <NavLink className={window.location.pathname === item.path ? "active" : ""} href={item.path}>{item.name}</NavLink> 
+                    </DropdownItem>
+                  ))}
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            :
+              <NavItem key={index} >
+                <NavLink className={window.location.pathname === ele.path ? "active" : ""} href={ele.path}>{ele.name}</NavLink>
+              </NavItem>
+          ))}
+        </Nav>
+        <div className="lang_btn_container">
+          <Button className='lang_btn' onClick={toggleLangModal}>
+            {Text.lang.button}
+            <i className="fa-solid fa-globe"></i>
+          </Button>
+        </div>
+      </Collapse>
+    </Navbar>
+  )
+
   return (
-    <div className='vision_header'>
-      <Navbar {...args} fixed='top' expand='lg'>
-        <NavbarBrand href="/">
-          <img className='logo' src={Logo} alt='TALENT VISION CARE' />
-        </NavbarBrand>
-        <NavbarToggler onClick={toggle} />
-        <Collapse isOpen={isOpen} navbar>
-          <Nav className="m-auto" navbar>
-            {Text.nav_links.map((ele, index) => (
-              ele.drop_down ?
-                <UncontrolledDropdown nav inNavbar>
-                  <DropdownToggle nav caret>
-                    {ele.name}
-                  </DropdownToggle>
-                  <DropdownMenu right>
-                    {ele.routes.map((item, index) => (
-                      <DropdownItem key={index}>
-                        <NavLink className={window.location.pathname === item.path ? "active" : ""} href={item.path}>{item.name}</NavLink> 
-                      </DropdownItem>
-                    ))}
-                  </DropdownMenu>
-                </UncontrolledDropdown>
-              :
-                <NavItem key={index} >
-                  <NavLink className={window.location.pathname === ele.path ? "active" : ""} href={ele.path}>{ele.name}</NavLink>
-                </NavItem>
-            ))}
-          </Nav>
-          <div className="lang_btn_container">
-            <Button className='lang_btn' onClick={toggleLangModal}>
-              {Text.lang.button}
-              <i className="fa-solid fa-globe"></i>
-            </Button>
-          </div>
-        </Collapse>
-      </Navbar>
+    <header>
+      {renderBody()}
       {/* Language Modal */}
       <Modal isOpen={modal} toggle={toggleLangModal} {...args}>
         <ModalHeader toggle={toggleLangModal}>{Text.lang.select}</ModalHeader>
@@ -100,7 +104,7 @@ function Header(args) {
         ))}
         </ModalBody>
       </Modal>
-    </div>
+    </header>
   );
 }
 
