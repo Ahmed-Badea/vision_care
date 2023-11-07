2a0134f0e495f37b82427d053a242f973081c1f1
Ahmed-Badea <ahmedbadea158@gmail.com>
2023-11-07 23:00:14 +0200

    handle cutomer sustainable development comments

--- src/Pages/SustainableDevelopment/SustainableDevelopment.js
+++ src/Pages/SustainableDevelopment/SustainableDevelopment.js
@@ -1,26 +1,26 @@
 import React from 'react'
 import { Container } from 'reactstrap';
-import {dependencies} from '../../Tools/dependencies'
-import EyeIcon from './../../assets/images/eye.jpg'
-import text from './text.json'
+// import {dependencies} from '../../Tools/dependencies'
+import developIcon from './../../assets/images/develop.jpg'
+// import text from './text.json'
 import 'animate.css';
 import './SustainableDevelopment.scss'
 
 function SustainableDevelopment() {
 
-  const currentLang = dependencies.getCurrentLang();
-  const Text = text[currentLang];
+  // const currentLang = dependencies.getCurrentLang();
+  // const Text = text[currentLang];
 
   const renderBody = () => (
     <div className='development_container'>
       <Container>
-        <img src={EyeIcon} alt="Eye" className='animate__animated animate__backInDown' />
-        <div className='description'>
+        <img src={developIcon} alt="Eye" className='animate__animated animate__backInDown' />
+        {/*<div className='description'>
           <p>{Text.description}</p>
           <p>{Text.description}</p>
           <p>{Text.description}</p>
           <p>{Text.description}</p>
-        </div>
+        </div>*/}
       </Container>
     </div>
   )
