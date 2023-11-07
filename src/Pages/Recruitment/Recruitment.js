9eaaf093ffe3b47a4e24e2699e3b2998b18d2951
Ahmed-Badea <ahmedbadea158@gmail.com>
2023-11-07 23:11:56 +0200

    handle cutomer recrruitment comments

--- src/Pages/Recruitment/Recruitment.js
+++ src/Pages/Recruitment/Recruitment.js
@@ -1,15 +1,57 @@
 import React from 'react'
+import { Container, Row, Col } from 'reactstrap';
 import {dependencies} from '../../Tools/dependencies'
 import text from './text.json'
+import './../ContactUs/ContactUs.scss'
 
 function Recruitment() {
 
   const currentLang = dependencies.getCurrentLang();
   const Text = text[currentLang];
 
+  const renderBody = () => (
+    <Container> 
+      <Row>
+        <Col lg={4}>
+          <div className='box animate__animated animate__rollIn'>
+            <div className='icon'>
+              <i className="fa fa-linkedin"></i> 
+            </div>
+            <p>{Text.linkedin}</p>
+            <a href="mailto:info@visioncareksa.com">
+              <span>info@visioncareksa.com</span>
+            </a>
+          </div>
+        </Col>
+        <Col lg={4}>
+          <div className='box animate__animated animate__rollIn'>
+            <div className='icon'>
+              <i className="fa fa-phone"></i> 
+            </div>
+            <p>{Text.phone}</p>
+            <a href="tel:+966 11 207 9919">
+              <span>+966 11 207 9919</span>
+            </a>
+          </div>
+        </Col>
+        <Col lg={4}>
+          <div className='box animate__animated animate__rollIn'>
+            <div className='icon'>
+              <i className="fa fa-envelope"></i> 
+            </div>
+            <p>{Text.email}</p>
+            <a href="mailto:info@visioncareksa.com">
+              <span>info@visioncareksa.com</span>
+            </a>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  )
+
   return (
-    <div style={{"height": "500px", "textAlign": "center", "lineHeight": "500px", "fontSize": "30px"}}>
-      {Text.recruitment}
+    <div className='contact_us_container'>
+      {renderBody()}
     </div>
   )
 }
