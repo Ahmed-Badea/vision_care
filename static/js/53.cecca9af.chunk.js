40a61c689457b9e782a3e15ed36330d277088313
Ahmed-Badea <ahmedbadea158@gmail.com>
2023-11-08 21:25:10 +0200

    HashRouter build

---
+++ static/js/53.cecca9af.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkvision_care=self.webpackChunkvision_care||[]).push([[53],{2e3:function(e,n,a){a.r(n),a.d(n,{default:function(){return t}});a(2791);var c=a(7823),i=a.p+"static/media/develop.6e3843c6ccb09ee478e3.jpg",s=a(184);var t=function(){return(0,s.jsx)("div",{children:(0,s.jsx)("div",{className:"development_container",children:(0,s.jsx)(c.Z,{children:(0,s.jsx)("img",{src:i,alt:"Eye",className:"animate__animated animate__backInDown"})})})})}}}]);
+//# sourceMappingURL=53.cecca9af.chunk.js.map
